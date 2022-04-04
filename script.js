d7217b983cfd79ef2e029a1255880355da775b40
sulabh007 <66020896+sulabh007@users.noreply.github.com>
2022-04-04 21:20:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,22 +4,31 @@ document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('#create1').addEventListener('click', function () {
         create_inp();
     })
-})
+});
 
+
+//To load every task in list form
 function load_task(){
     document.getElementById('Add_to-do').style.display='none';
     document.getElementById('edit_to-do').style.display = 'none';
     document.getElementById('task-detail').style.display = 'none';
-    
+
+    //parse converts JSON from string to array obj
     var info = localStorage.getItem("todo");
     var parsedobj =[];
     if (info !== null) {
         parsedobj = JSON.parse(info);
     }
-   
-    console.log(parsedobj.length, parsedobj);
+
+    
+    document.querySelector(".delete-all").addEventListener('click', () => {
+        if (confirm("All Task will be deleted, Are you sure?")){
+            delete_all();
+        }
+        
+    });
+
     for(let i=0;i<parsedobj.length;i++){
-        console.log(parsedobj[i]);
         if(parsedobj[i]!==null){
             
             const task = document.createElement('div');
@@ -30,20 +39,32 @@ function load_task(){
             if (!parsedobj[i].completed) {  
                 document.querySelector('.not_comp').appendChild(task);
             }   
-            else{ 
+            else{
+                task.innerHTML +='&#10004' ;
                 document.querySelector('.comp').appendChild(task);  
             }
             task.addEventListener('click', () => task_details(i));
-            console.log(parsedobj[i].type);
         }
     }
 }
 
-
+//create task
 function create_inp(){
     document.getElementById('Add_to-do').style.display = 'block';
     document.getElementById('task-detail').style.display = 'none';
     document.getElementById('edit_to-do').style.display = 'none';
+    
+
+    //so no empty task gets added
+    document.querySelector('#task-submit').disabled = true;
+    document.querySelector('#add-task-name').onkeyup = () => {
+        if (document.querySelector('#add-task-name').value.length > 0) {
+            document.querySelector('#task-submit').disabled = false;
+        }
+        else {
+            document.querySelector('#task-submit').disabled = true;
+        }
+    }
 
     let to_do_arr = [];
     let task_dic = {
@@ -56,21 +77,17 @@ function create_inp(){
         'Date': "",
         'Day': ""
     };
-
+    
     const currtime = new Date();
     var time = currtime.toLocaleString('en-US', { hour: 'numeric', minute: "numeric", hour12: false })
-    console.log(time);
     var date = new Date();
     let todo = localStorage.getItem("todo");
     if (todo !== null) {
         to_do_arr = JSON.parse(todo);
     }
-    console.log(document.querySelector('#submit'))
-    console.log(document.querySelectorAll(".add-task"))
-    document.querySelector('#submit').addEventListener('click', function () {
+    document.querySelector('#task-submit').addEventListener('click', function () {
         const task = document.querySelectorAll(".add-task");
         const detail = document.querySelector('#add_details');
-        console.log('ls',to_do_arr,to_do_arr.length);
         task_dic = {"Task": task[0].value,
                     "CreatedAt": time,
                     "Date": date.toISOString().split('T')[0],
@@ -80,13 +97,10 @@ function create_inp(){
                     "Details": detail.value,
                     "type": task[2].value
                     }
-        console.log(task_dic, to_do_arr.length);
         to_do_arr.push(task_dic);
         localStorage.setItem("todo", JSON.stringify(to_do_arr));
         task_details(to_do_arr.length-1)
-        location.reload();
-        
-        
+        location.reload();  
         return false;
     })
     
@@ -109,18 +123,15 @@ function task_details(i){
     else {
         parsedobj = JSON.parse(info);
     }
-    console.log(parsedobj)
-    console.log(i, parsedobj[i].Deadline);
     var deadline = `${parsedobj[i].Deadline.split('T')[0]} ${parsedobj[i].Deadline.split('T')[1]}`;
-    console.log(deadline);
     if (parsedobj[i].Deadline==""){
         deadline="None"
     }
     const task = document.querySelector(".task")
     task.innerHTML = `<p class='task_name'><b>${parsedobj[i].Task}</b></p>
                     <p class='task_deadline'><span class="task_dets">Deadline: </span>${deadline}</p>
+                    <p class='task_type'><span class="task_dets">Type: </span>${parsedobj[i].type}</p>
                     <p class='task_details'>${(parsedobj[i].Details !== undefined) ? parsedobj[i].Details : ""}</p>
-                    <p class='task_type'>${parsedobj[i].type}</p>
                     <p class='task_time'>${parsedobj[i].CreatedAt}</p>
                     <p class='task_time'>${parsedobj[i].Date}</p>
                     <p class='task_time'>${parsedobj[i].Day}</p>`;
@@ -132,16 +143,16 @@ function task_details(i){
     
 
     if(parsedobj[i].completed){
-        document.querySelector(".bttn_edit").style.display="none";
+        document.querySelector("#bttn_edit").style.display="none";
     }
    
     document.querySelector(".Done").addEventListener('click', () => {
         task_completed(i);
     });
-    document.querySelector(".bttn_edit").addEventListener('click', () => {
+    document.querySelector("#bttn_edit").addEventListener('click', () => {
         edit_task(i);
     });
-    document.querySelector(".bttn_delete").addEventListener('click', () => {
+    document.querySelector("#bttn_delete").addEventListener('click', () => {
         delete_task(i);
     });
 }
@@ -157,20 +168,19 @@ function edit_task(i){
     document.getElementById('task-detail').style.display = 'none';
     document.getElementById('edit_to-do').style.display = 'block';
 
-    console.log(document.querySelectorAll('.edit_task'));
     var task=JSON.parse(localStorage.todo)
     const edit_task = document.querySelectorAll('.edit_task');
     edit_task[0].value = task[i].Task;
     edit_task[3].value = task[i].type;
     edit_task[1].value = task[i].Details;
     edit_task[2].value = task[i].Deadline;
-    document.querySelector("#submit1").addEventListener('click', () => {
+    
+    document.querySelector("#edit-submit").addEventListener('click', () => {
         task[i].Task = edit_task[0].value;
         task[i].type = edit_task[3].value;
         task[i].Details = edit_task[1].value;
         task[i].Deadline = edit_task[2].value;
         localStorage.setItem("todo",JSON.stringify(task));
-        console.log(task);
         task_details(i);
     });
     
@@ -178,7 +188,6 @@ function edit_task(i){
 
 function delete_task(i){
     var task = JSON.parse(localStorage.todo)
-    //console.log(task, "//", task.length, task[0].Task)
     delete task[i];
     localStorage.setItem("todo", JSON.stringify(task));
     location.reload();
@@ -195,15 +204,26 @@ function task_completed(i) {
     if(!parsedobj[i].completed){
         parsedobj[i].completed = true;
         clsname.textContent =  "Restore Task"
-        document.querySelector(".bttn_edit").style.display="none";
+        document.querySelector("#bttn_edit").style.display="none";
     }
     else{
         parsedobj[i].completed = false;
         clsname.textContent = "Mark as Done" ;
-        document.querySelector(".bttn_edit").style.display = "block";
+        document.querySelector("#bttn_edit").style.display = "block";
     }
-    console.log(parsedobj[i]);
     localStorage.setItem('todo', JSON.stringify(parsedobj));
     location.reload();
     load_task();
+}
+
+function delete_all(){
+    var info = localStorage.getItem("todo");
+    if (info!=='[]') {
+        let parse=[];
+        localStorage.setItem('todo', JSON.stringify(parse));
+        location.reload();
+    }
+    else{
+         return alert("There's nothing to delete");
+    }  
 }
